e6fe055fb614fe030f5a27cdae10e281b684d5ec
LachlanMcL <zombieljm@yahoo.com>
2024-03-10 09:27:43 +1100

    single alien GET request params added

--- API.js
+++ API.js
@@ -13,25 +13,7 @@ connectToDb((err) => {
 })
 
 app.get('/aliens', (req, res) => { 
-    //check if specific parameters are requested
-    const parameters = {
-        name: req.query.name || true,
-        species: req.query.species || true,
-        home_world: req.query.home_world || true,
-        abilities:req.query.abilities || true,
-        image: req.query.image || true
-    }
-
-    for (let key of Object.keys(parameters)) { //remove parameter if it doesnt === "false". We will only specifiy what params NOT to return later.
-        let param = parameters[key]
-        if (param !== "false") {
-            delete parameters[key]
-            continue
-        }
-        parameters[key] = 0
-    }
-
-    parameters._id = 0
+    const parameters = checkParameters(req)
 
     let aliens = []
 
@@ -48,9 +30,11 @@ app.get('/aliens', (req, res) => {
 })
 
 app.get('/aliens/:id', (req, res) => { 
-    
+
+    const parameters = checkParameters(req)
+
     db.collection('aliens')
-    .findOne({name: req.params.id})
+    .findOne({name: req.params.id}, {projection: parameters})
     .then(alien => {
         res.status(200).json(alien)
     })
@@ -58,5 +42,28 @@ app.get('/aliens/:id', (req, res) => {
         res.status(500).json({error: 'could not fetch doc'})
     })
 })
+
+function checkParameters(req) {
+    //check if specific parameters are requested
+    const parameterList = {
+        name: req.query.name || true,
+        species: req.query.species || true,
+        home_world: req.query.home_world || true,
+        abilities:req.query.abilities || true,
+        image: req.query.image || true
+    }
+
+    for (let key of Object.keys(parameterList)) { //remove parameter if it doesnt === "false". We will only specifiy what params NOT to return later.
+        let param = parameterList[key]
+        if (param !== "false") {
+            delete parameterList[key]
+            continue
+        }
+        parameterList[key] = 0
+    }
+
+    parameterList._id = 0
+    return parameterList
+}
 
 module.exports = {app}
